f5efac1ad7f93d1e1203ddb2312143a9c44a5875
Endle, Cory M <Endle.Cory@mayo.edu>
2019-08-12 14:02:51 -0500

    Initial commit.

---
+++ artifact-builder/test.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+# Set the PATH env. variable
+export PATH=${PATH}:/nlp2fhir_lib:/resources
+
+# MedTagger
+echo "*************************************************************************"
+echo "Cloning git repository $MED_TAGGER_REPO from branch $MED_TAGGER_BRANCH"
+echo "*************************************************************************"
+
+git clone -b $MED_TAGGER_BRANCH $MED_TAGGER_REPO && \
+    cd MedTagger/ && \
+    mvn clean install && \
+    # Copy artifacts of build to /nlp2fhir_lib directory
+    cp target/*.jar /nlp2fhir_lib && \
+    # Copy resources to the resources and root directories
+    cp -R src/main/resources/* /resources
+    cp -R src/main/resources/* /target
+    cd ..
+
+# MedTIME
+echo "*************************************************************************"
+echo "Cloning git repository $MED_TIME_REPO from branch $MED_TIME_BRANCH"
+echo "*************************************************************************"
+
+git clone -b $MED_TIME_BRANCH $MED_TIME_REPO && \
+    cd MedTime/ && \
+    mvn clean install && \
+    # Copy artifacts of build to /nlp2fhir_lib directory
+    cp target/*.jar /nlp2fhir_lib && \
+    # Copy resources to the resources and root directories
+    cp -R src/main/resources/* /resources
+    cp -R src/main/resources/* /target
+    cd ..
+
+# MedXN
+echo "*************************************************************************"
+echo "Cloning git repository $MED_XN_REPO from branch $MED_XN_BRANCH"
+echo "*************************************************************************"
+
+git clone -b $MED_XN_BRANCH $MED_XN_REPO && \
+    cd MedXN/ && \
+    mvn clean install && \
+    # Copy artifacts of build to /nlp2fhir_lib directory
+    cp target/*.jar /nlp2fhir_lib && \
+    # Copy resources to the resources and root directories
+    cp -R src/main/resources/* /resources
+    cp -R src/main/resources/* /target
+    cd ..
+
+# Copy all desc directories to the main level.  This will fix classpath issues.
+cp -R /resources/desc/* /target
+
+# UMLS_VTS
+echo "*************************************************************************"
+echo "Cloning git epository $UMLS_VTS_REPO from branch $UMLS_VTS_BRANCH"
+echo "*************************************************************************"
+
+git clone -b $UMLS_VTS_BRANCH $UMLS_VTS_REPO && \
+    cd UMLS_VTS/ && \
+    mvn clean install && \
+    # Copy artifacts of build to /nlp2fhir_lib directory
+    cp target/*.jar /nlp2fhir_lib
+    cd ..
+
+# UIMA_STREAM_SERVER
+echo "*************************************************************************"
+echo "Cloning git repository $UIMA_STREAM_SERVER_REPO from branch $UIMA_STREAM_SERVER_BRANCH"
+echo "*************************************************************************"
+git clone -b $UIMA_STREAM_SERVER_BRANCH $UIMA_STREAM_SERVER_REPO && \
+    cd UIMA-Stream-Server/ && \
+    mvn clean install && \
+    # Copy artifacts of build to /nlp2fhir_lib directory
+    cp UIMA-Server-Core/target/*.jar /nlp2fhir_lib && \
+    cp UIMA-Server-Plugins/ExampleServerPlugin/target/*.jar /nlp2fhir_lib && \
+    cp UIMA-Server-REST/target/*.jar /nlp2fhir_lib && \
+    cd ..
+
+
+# cTAKES
+echo "*************************************************************************"
+echo "Downloading cTAKES reources package... this will take a few minutes."
+echo "*************************************************************************"
+CTAKES_DIR='cTAKES'
+mkdir $CTAKES_DIR && cd $CTAKES_DIR && \
+    wget http://sourceforge.net/projects/ctakesresources/files/ctakes-resources-4.0-bin.zip && \
+    unzip ctakes-resources-4.0-bin.zip
+    cp -r resources/* /resources
+    # Copy resource to additional directory for classpath issues at runtime.
+    cp -r resources/org/apache /target/org/
+    cd ..
+
+# NLP2FHIR
+echo "*************************************************************************"
+echo "Cloning git repository $NLP2FHIR_REPO from branch $NLP2FHIR_BRANCH"
+echo "*************************************************************************"
+echo "path =  $PATH"
+
+git clone -b $NLP2FHIR_BRANCH $NLP2FHIR_REPO && \
+      cd NLP2FHIR/ && \
+      mvn clean install && \
+      # Copy artifacts of build to /nlp2fhir_lib directory
+      cp AnnotationUtils/target/*.jar /nlp2fhir_lib
+      cp FHIR-Typesystem-UIMA/target/*.jar /nlp2fhir_lib
+      cp NLP2FHIR-GUI/target/*.jar /nlp2fhir_lib
+      cp NLP2FHIR-STREAM/target/*.jar /nlp2fhir_lib
+      cp NLP2FHIR-WEB/target/*.war /nlp2fhir_lib
+      cd ..
+
+#*****************************************************************
+# Change permission of /nlp2fhir_lib jars and /resources
+# to be executable.
+#*****************************************************************
+chmod -R 755 /nlp2fhir_lib
+chmod -R 755 /resources
---
+++ test.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+#*****************************************************************
+# Main script to build BD2KOnFHIR/NLP2FHIR.
+# https://github.com/BD2KOnFHIR/NLP2FHIR
+# This script will create a Docker container to pull and build the
+# multiple software projects required and place them in a ./target
+# diretory.
+#
+# Requirement:  UMLS requires a user license.  As such, it is
+# the responsibility of the user to manually download the UMLS and
+# and SNOMEDCT_US.
+#
+#  - The MRCONSO.RRF file from a copy of the UMLS (placed in ./UMLS)
+#  - SNOMEDCT US Edition resource files (downloadable with a UMLS
+#    license, placed in ./SNOMEDCT_US)
+#
+#*****************************************************************
+
+ROOT_DIR=$(pwd)
+
+# Environment Variables
+UMLS_VTS_BRANCH=master
+UMLS_VTS_REPO=https://github.com/OHNLPIR/UMLS_VTS.git
+
+MED_TAGGER_BRANCH=master
+MED_TAGGER_REPO=https://github.com/OHNLPIR/MedTagger.git
+
+MED_TIME_BRANCH=master
+MED_TIME_REPO=https://github.com/OHNLPIR/MedTime.git
+
+MED_XN_BRANCH=master
+MED_XN_REPO=https://github.com/ohnlp/MedXN.git
+
+NLP2FHIR_BRANCH=master
+NLP2FHIR_REPO=https://github.com/BD2KOnFHIR/NLP2FHIR.git
+
+UIMA_STREAM_SERVER_BRANCH=master
+UIMA_STREAM_SERVER_REPO=https://github.com/OHNLPIR/UIMA-Stream-Server.git
+
+#*****************************************************************
+# Output the variables being used
+#*****************************************************************
+echo
+echo "*** Environment Variables ***"
+echo
+echo UMLS_VTS_BRANCH : $UMLS_VTS_BRANCH
+echo UMLS_VTS_REPO   : $UMLS_VTS_REPO
+echo
+echo MED_TAGGER_BRANCH : $MED_TAGGER_BRANCH
+echo MED_TAGGER_REPO   : $MED_TAGGER_REPO
+echo
+echo MED_TIME_BRANCH : $MED_TIME_BRANCH
+echo MED_TIME_REPO   : $MED_TIME_REPO
+echo
+echo MED_XN_BRANCH : $MED_XN_BRANCH
+echo MED_XN_REPO   : $MED_XN_REPO
+echo
+echo UIMA_STREAM_SERVER_BRANCH: $UIMA_STREAM_SERVER_BRANCH
+echo UIMA_STREAM_SERVER_REPO  : $UIMA_STREAM_SERVER_REPO
+echo
+echo NLP2FHIR_BRANCH : $NLP2FHIR_BRANCH
+echo NLP2FHIR_REPO   : $NLP2FHIR_REPO
+echo
+#*****************************************************************
+# Remove target directories and recreate them fresh.
+#*****************************************************************
+rm -rf $ROOT_DIR/target
+
+mkdir $ROOT_DIR/target
+mkdir $ROOT_DIR/target/artifacts
+mkdir $ROOT_DIR/target/artifacts/lib
+mkdir $ROOT_DIR/target/artifacts/resources
+
+#*****************************************************************
+# Verify that the ./UMLS and ./SNOMEDCT_US directories exist
+# and are populated.
+#*****************************************************************
+FILE=""
+DIR_UMLS="UMLS"
+DIR_SNOMED="SNOMEDCT_US"
+
+# look for empty ./UMLS dir
+if [ "$(ls -A $DIR_UMLS)" ]; then
+     echo "$DIR_UMLS directory is not Empty"
+     echo ""
+else
+    echo "The $DIR_UMLS directory is Empty"
+    echo "The MRCONSO.RRF file from a copy of the UMLS needs to be placed in the ./UMLS directory."
+    echo ""
+    exit
+fi
+
+# look for empty ./SNOMEDCT_US dir
+if [ "$(ls -A $DIR_SNOMED)" ]; then
+     echo "$DIR_SNOMED directory is not Empty"
+     echo ""
+else
+    echo "The $DIR_SNOMED directory is Empty"
+    echo "The SNOMEDCT US Edition resource files (downloadable with a UMLS license, need to be placed in the ./SNOMEDCT_US directory."
+    echo ""
+    exit
+fi
+
+#*****************************************************************
+# Create a maven container
+#*****************************************************************
+MAVEN_CONTAINER=$(docker run -d -P --name maven -v ~/.m2:/root/.m2:rw ubuntu)
+
+#*****************************************************************
+# Artifact builder will build MedTagger, cTAKES, MedTime,
+# MedXN, and UMLS_VTS
+#*****************************************************************
+cd artifact-builder
+docker build -t artifact-builder .
+docker run --rm \
+  -v $ROOT_DIR/target:/target \
+  -v $ROOT_DIR/target/artifacts/lib:/nlp2fhir_lib \
+  -v $ROOT_DIR/target/artifacts/resources:/resources \
+  -e UMLS_VTS_BRANCH=$UMLS_VTS_BRANCH -e UMLS_VTS_REPO=$UMLS_VTS_REPO \
+  -e MED_TAGGER_BRANCH=$MED_TAGGER_BRANCH -e MED_TAGGER_REPO=$MED_TAGGER_REPO \
+  -e MED_TIME_BRANCH=$MED_TIME_BRANCH -e MED_TIME_REPO=$MED_TIME_REPO \
+  -e MED_XN_BRANCH=$MED_XN_BRANCH -e MED_XN_REPO=$MED_XN_REPO \
+  -e UIMA_STREAM_SERVER_BRANCH=$UIMA_STREAM_SERVER_BRANCH -e UIMA_STREAM_SERVER_REPO=$UIMA_STREAM_SERVER_REPO \
+  -e NLP2FHIR_BRANCH=$NLP2FHIR_BRANCH -e NLP2FHIR_REPO=$NLP2FHIR_REPO \
+  --volumes-from maven artifact-builder
+cd ..
+
+#*****************************************************************
+# Remove maven container
+#*****************************************************************
+docker stop $MAVEN_CONTAINER
+docker rm $MAVEN_CONTAINER
