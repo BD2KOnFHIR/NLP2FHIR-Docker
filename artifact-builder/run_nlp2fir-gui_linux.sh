c0426ed6d9df286318f8b2a5e55c8745c8bb3c95
Endle, Cory M <Endle.Cory@mayo.edu>
2019-10-23 07:50:56 -0500

    updated memory usage on the gui shell for linux/mac

--- artifact-builder/run_nlp2fir-gui_linux.sh
+++ artifact-builder/run_nlp2fir-gui_linux.sh
@@ -1,1 +1,1 @@
-java -cp "NLP2FHIR-GUI-1.0-SNAPSHOT.jar:./*:artifacts/lib/*:artifacts/resources" edu.mayo.bsi.nlp2fhir.gui.GUI
+java -Xmx4000m -cp "NLP2FHIR-GUI-1.0-SNAPSHOT.jar:./*:artifacts/lib/*:artifacts/resources" edu.mayo.bsi.nlp2fhir.gui.GUI
